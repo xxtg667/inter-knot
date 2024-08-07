1dc58dffdde99b905cb4a4127081a5219991029f
share121 <2854631158@qq.com>
2024-08-07 19:22:20 +0800

    尝试获取访问令牌并处理失败情况
    引入错误处理以在获取访问令牌时捕获任何潜在的异常，并适当地响应。之前，代码缺乏处理获取令牌过程中可能发生的错误的机制。此更改确保在获取令牌失败时执行备用逻辑，提高代码的健壮性。

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,11 @@ LA.init({
 (async () => {
   if (!(localStorage.getItem("access_token")?.startsWith("ghu_") ?? false)) {
     if (new URL(location.href).searchParams.has("code")) {
-      await getAccessToken(new URL(location.href).searchParams.get("code")!);
+      try {
+        await getAccessToken(new URL(location.href).searchParams.get("code")!);
+      } catch {
+        getCode();
+      }
     } else {
       getCode();
     }
@@ -229,17 +233,18 @@ LA.init({
     // }).then((e) => e.json());
     const { data } = await graphql({
       access_token,
-      data: `
-        query {
-          viewer {
-            avatarUrl
-            name
-            repositories {
-              totalCount
+      data: JSON.stringify({
+        query: `
+          query {
+            viewer {
+              avatarUrl
+              name
+              repositories {
+                totalCount
+              }
             }
-          }
-        }
-      `,
+          }`,
+      }),
     });
     return {
       name: data.viewer.name,
