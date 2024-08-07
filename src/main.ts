6d036909abf659b7a6f5b94beb898b78c9f4e783
share121 <2854631158@qq.com>
2024-08-07 10:19:05 +0800

    "实现Macy的按需重计算以优化性能
    修改了Macy配置的初始化方式，并在图片加载后触发重计算，以确保布局正确更新。通过直接操作DOM并利用事件监听器，实现了资源加载和布局管理的解耦，从而提升了页面性能。"

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ tabs.forEach((e) => {
     e.classList.add("active");
   });
 });
-Macy({
+const macy = Macy({
   container: ".card-container",
   margin: {
     x: 0,
@@ -92,4 +92,7 @@ function renderArticleList(
         `<div class="card-wrapper"><div class="card"><section class="cover-container"><img class="cover" src="${cover}" alt="封面" /><div class="visited"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" ><path fill="currentColor" d="M1.182 12C2.122 6.88 6.608 3 12 3s9.878 3.88 10.819 9c-.94 5.12-5.427 9-10.819 9s-9.878-3.88-10.818-9M12 17a5 5 0 1 0 0-10a5 5 0 0 0 0 10m0-2a3 3 0 1 1 0-6a3 3 0 0 1 0 6" /></svg>${visited}</div></section><section class="info-container"><div class="profile"><img class="profile-photo" src="${authorPhoto}" alt="头像" /><div class="username">${author}</div></div><div class="title">${title}</div><div class="content">${content}</div></section></div></div>`
     )
     .join("");
+  document.querySelectorAll(".card-container img").forEach((e) => {
+    e.addEventListener("load", () => macy.recalculate(true), { once: true });
+  });
 }
