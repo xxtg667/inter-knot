66cfaae592a86a7a957704933ae5e2fb3b8ca59b
share121 <2854631158@qq.com>
2024-08-08 08:59:41 +0800

    添加文章链接并优化弹出窗口的显示
    在文章列表和讨论数据中添加了URL字段，以便在弹出窗口中提供链接。同时，简化了弹出窗口的渲染逻辑，并在UI中添加了回复链接，使用户可以更方便地访问和互动。

--- src/main.ts
+++ src/main.ts
@@ -75,6 +75,7 @@ window.run = async () => {
         author: e.author.login,
         content: e.bodyHTML,
         text: e.bodyText,
+        url: e.url,
         visited: getRandomInt(0, 1001),
         comments: e.comments.nodes.map((e) => ({
           profilePhoto: e.author.avatarUrl,
@@ -165,6 +166,7 @@ function renderArticleList(
     content: string;
     visited: number;
     text: string;
+    url: string;
     comments: {
       profilePhoto: string;
       name: string;
@@ -184,18 +186,8 @@ function renderArticleList(
     })
   );
   document.querySelectorAll(".card-container .card").forEach((e, i) => {
-    const { cover, title, author, authorPhoto, content, visited, comments } =
-      list[i];
     e.addEventListener("click", () => {
-      renderPopup({
-        cover,
-        title,
-        author,
-        authorPhoto,
-        content,
-        visited,
-        comments,
-      });
+      renderPopup(list[i]);
     });
   });
 }
@@ -208,6 +200,7 @@ function renderPopup({
   title,
   content,
   comments,
+  url,
 }: {
   authorPhoto: string;
   author: string;
@@ -215,13 +208,14 @@ function renderPopup({
   cover: string;
   title: string;
   content: string;
+  url: string;
   comments: {
     profilePhoto: string;
     name: string;
     content: string;
   }[];
 }) {
-  popupContainer.innerHTML = `<div class="popup"><header><img class="profile-photo" src="${authorPhoto}" alt="头像" /><div><div>${author}</div><div class="visited"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" ><path fill="currentColor" d="M1.182 12C2.122 6.88 6.608 3 12 3s9.878 3.88 10.819 9c-.94 5.12-5.427 9-10.819 9s-9.878-3.88-10.818-9M12 17a5 5 0 1 0 0-10a5 5 0 0 0 0 10m0-2a3 3 0 1 1 0-6a3 3 0 0 1 0 6" /></svg>${visited}</div></div><div class="close-btn"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" ><path fill="currentColor" d="m12 10.587l4.95-4.95l1.414 1.414l-4.95 4.95l4.95 4.95l-1.415 1.414l-4.95-4.95l-4.949 4.95l-1.414-1.415l4.95-4.95l-4.95-4.95L7.05 5.638z" /></svg></div></header><main><div class="cover"><img src="${cover}" alt="封面" /></div><div class="content"><div class="title">${title}</div><div class="text">${content}</div><div class="comments">${comments
+  popupContainer.innerHTML = `<div class="popup"><header><img class="profile-photo" src="${authorPhoto}" alt="头像" /><div><div>${author}</div><div class="visited"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" ><path fill="currentColor" d="M1.182 12C2.122 6.88 6.608 3 12 3s9.878 3.88 10.819 9c-.94 5.12-5.427 9-10.819 9s-9.878-3.88-10.818-9M12 17a5 5 0 1 0 0-10a5 5 0 0 0 0 10m0-2a3 3 0 1 1 0-6a3 3 0 0 1 0 6" /></svg>${visited}</div></div><div class="close-btn"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" ><path fill="currentColor" d="m12 10.587l4.95-4.95l1.414 1.414l-4.95 4.95l4.95 4.95l-1.415 1.414l-4.95-4.95l-4.949 4.95l-1.414-1.415l4.95-4.95l-4.95-4.95L7.05 5.638z" /></svg></div></header><main><div class="cover"><img src="${cover}" alt="封面" /></div><div class="content"><div class="title">${title}</div><div class="text">${content}</div><a class="reply" href="${url}"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M9.243 18.997H21v2H3v-4.243l9.9-9.9l4.242 4.243zm5.07-13.557l2.122-2.121a1 1 0 0 1 1.414 0l2.829 2.828a1 1 0 0 1 0 1.415l-2.122 2.121z"></path></svg>写回复</a><div class="comments">${comments
     .map(
       (e) =>
         `<section class="comment"><img class="profile-photo" src="${e.profilePhoto}" alt="头像" /><div><div class="name">${e.name}</div><div class="text">${e.content}</div></div></section>`
@@ -320,6 +314,7 @@ async function getDiscussions(access_token: string) {
             bodyHTML: string;
             bodyText: string;
             title: string;
+            url: string;
             comments: {
               nodes: {
                 author: {
@@ -336,7 +331,7 @@ async function getDiscussions(access_token: string) {
     };
   } = await graphql({
     access_token,
-    data: 'query { repository(owner: "share121", name: "inter-knot") { discussions(first: 100) { nodes { author { avatarUrl login url } bodyHTML bodyText title comments(first: 100) { nodes { author { avatarUrl login url } bodyHTML } } } } } }',
+    data: 'query { repository(owner: "share121", name: "inter-knot") { discussions(first: 100) { nodes { author { avatarUrl login url } bodyHTML url bodyText title comments(first: 100) { nodes { author { avatarUrl login url } bodyHTML } } } } } }',
   });
   return nodes;
 }
